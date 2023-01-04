915c26dc3d82f920a078dba456782afad54f638e
seongju <lsj40413@gmail.com>
2023-01-04 23:17:23 +0900

    Feat: "Solve Ex4"
    "파보나치 수열" 문제풀이

--- src/main/java/section2/Ex4.java
+++ src/main/java/section2/Ex4.java
@@ -7,40 +7,18 @@ import java.util.Scanner;
 
 public class Ex4 {
 
-    // 에라토스테네스 체 알고리즘(소수 구하기.)
-    // 제곱근을 이용하면 for문의 개수를 줄일 수 있다.
-    // 하지만, 개수를 구하기 위해서 하나씩 적용하는 것이 더 빠름.
     public void solution(int n){
 
-        int[] primeArr = new int[n];
-        int cnt = 0;
+        int[] seq = new int[n];
 
-        if(n>1)
-            primeArr[0] = primeArr[1] = 1;
-
-
-        System.out.println(Math.sqrt(n));
-
-        for(int i=2; i<n; i++){
-            if(primeArr[i] == 0){
-                cnt++;
-                for(int j=i; j<n; j+=i){
-                    if(j != i)
-                        primeArr[j] = 1;
-                }
+        for(int i=0; i<seq.length; i++){
+            if(i == 0 || i==1)
+                seq[i] =1;
+            else{
+                seq[i] = seq[i-2] + seq[i-1];
             }
+            System.out.print(seq[i] + " ");
         }
-
-        for(int i=0; i<primeArr.length; i++){
-            if(primeArr[i] == 0)
-                System.out.print(i + " ");
-        }
-
-        System.out.println();
-        System.out.println(Arrays.toString(primeArr));
-        System.out.println(cnt);
-
-
     }
     public static void main(String args[]){
 
