8d41aeda805fb2e9f86212a323cb85befeaf57ba
seongju <lsj40413@gmail.com>
2023-01-15 21:44:36 +0900

    Feat: Solve Ex1
    "학급회장(해쉬)" 문제풀이
    
    HashMap 주요 메소드 정리.
    
    getOrDefault() 활용

--- src/main/java/section4/Ex1.java
+++ src/main/java/section4/Ex1.java
@@ -4,6 +4,14 @@ import java.util.*;
 
 public class Ex1 {
 
+    /**
+     HashMap 주요 메소드
+     map.getOrDefault(key, default): key의 value get 또는 default 리턴.
+     map.containsKey(key) : key의 존재여부 리턴
+     map.size(): key의 종류 개수.
+     map.remove(key) : 특정 key 삭제 후, 해당 key의 value값 리턴. (oldValue)
+      */
+
     public void solution(String s){
 
         HashMap<Character, Integer> map = new HashMap<>();
@@ -11,10 +19,18 @@ public class Ex1 {
         String answer = "";
 
         for(Character c : s.toCharArray()){
-            if(map.get(c) != null)
-                map.put(c, map.get(c) + 1);
-            else
-                map.put(c, 1);
+//            if(map.get(c) != null)
+//                map.put(c, map.get(c) + 1);
+//            else
+//                map.put(c, 1);
+
+            /**
+             * get()을 이용해서 카운트할 때 위처럼 if문 사용도 좋지만,
+             * geOrDefault()라는 메소드를 이용해서 한번에 가능.
+              */
+
+
+            map.put(c, map.getOrDefault(c, 0)  + 1);
         }
 
         List<Object> keyList = new ArrayList<>(map.keySet());
