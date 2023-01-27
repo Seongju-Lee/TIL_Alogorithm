8398f9a538e62c92cf5ad8af0f92936da05ea6c7
seongju <lsj40413@gmail.com>
2023-01-27 13:37:21 +0900

    Feat: Solved Ex10
    "미로탐색" 문제 재풀이
    
    x, y축 이동을 할 때, 확실히 정해놓고 -> 이동시키기.

--- src/main/java/section8/Ex10.java
+++ src/main/java/section8/Ex10.java
@@ -3,54 +3,62 @@ import java.util.*;
 
 public class Ex10 {
 
-    static int answer = 0;
+    static int[][] board;
+    static int cnt;
 
-    void DFS(int[][] grid, int nx, int ny, int vec) {
+    void DFS(int x, int y){
 
-        int[] xi = {0, 1, 0, -1};
-        int[] yi = {-1, 0, 1, 0};
+        int[] xi = {0,1,0,-1};
+        int[] yi = {-1,0,1,0};
 
-        if (ny == 7 && nx == 7) {
-//            System.out.println("answer: ");
-            answer++;
+        if(x == 6 && y == 6) {
+            cnt++;
+            return;
         }
 
-        for (int i = 0; i < 4; i++) {
+        for(int i=0; i<4; i++) {
 
-            int x = ny + yi[i];
-            int y = nx + xi[i];
+            int nx = x + xi[i];
+            int ny = y + yi[i];
 
-            if (grid[y][x] == 0 && x>=1 && x<=7 && y>=1 && y<=7) {
-//                System.out.println(ny + ", " + nx + ", i: " + i);
-                grid[y][x] = 1;
-                DFS(grid, x, y, i);
-                grid[y][x] = 0;
-            }
 
-        }
-    }
+            if(nx >= 0 && ny >= 0 && nx <=6 && ny <=6) {
 
-        public static void main (String[] args) {
-
-            Ex10 T = new Ex10();
-            Scanner sc = new Scanner(System.in);
-
-            int[][] grid = new int[9][9];
-
-            for (int i = 0; i <= 8; i++) {
-                for (int j = 0; j <= 8; j++) {
-
-                    if (i == 0 || i == 8 || j == 0 || j == 8)
-                        grid[i][j] = 1;
-                    else
-                        grid[i][j] = sc.nextInt();
+                if(board[ny][nx] == 0) {
+                    board[ny][nx] = 1;
+                    DFS(nx, ny);
+                    board[ny][nx] = 0;
                 }
             }
-
-            T.DFS(grid, 1, 1, 0);
-
-            System.out.println(answer);
         }
+
+
+
+
+        /**
+         * 상-우-하-좌 탐색 위해서 for문 4번 수행.
+         * 1. 탐색 했을 때 0이면 1로 변환하면서 해당 방향으로 이동.
+         * 2. DFS(x좌표, y좌표)
+         *
+         */
+
     }
+    public static void main(String[] args) {
 
+        Ex10 T = new Ex10();
+        Scanner sc = new Scanner(System.in);
 
+        board = new int[7][7];
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        board[0][0] = 1;
+        T.DFS(0,0);
+
+        System.out.println(cnt);
+    }
+}
