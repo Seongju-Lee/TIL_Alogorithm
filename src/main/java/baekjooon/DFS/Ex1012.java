73985fc19c96a8d9812bf087eb3c4efbd8d3e84b
seongju <lsj40413@gmail.com>
2023-02-06 20:34:49 +0900

    Feat: Solved Ex1012.java
    "유기농배추" 문제풀이
    
    DFS로 탐색하면서 상-우-좌-하 중 1이면 재귀를 진행하는 방식으로 풀이
    따로 visited[][] 배열을 사용하지 않고, 입력된 board[][] 배열에서 1을 지나치면 -1로 변환하는 방식으로 풀이 진행.

---
+++ src/main/java/baekjooon/DFS/Ex1012.java
@@ -0,0 +1,90 @@
+package baekjooon.DFS;
+
+import java.util.*;
+import java.io.*;
+
+
+class Ex1012 {
+    // 테스트케이스 개수만큼 main함수에 for문 돌림
+    // for문 안에 solution 메소드 실행
+
+    static int[][] board;
+    static int[] yi = {0, 1, 0, -1};
+    static int[] xi = {-1, 0, 1, 0};
+    static int n,m, cnt;
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    void DFS(int x, int y) {
+
+        board[x][y] = -1;
+
+        for(int k=0; k<4; k++) {
+
+            int nx = x + xi[k];
+            int ny = y + yi[k];
+
+            if(nx >=0 && ny>=0 && nx<m && ny<n && board[nx][ny] == 1) {
+
+                DFS(nx, ny);
+            }
+
+        }
+    }
+
+    void solution() {
+
+        for(int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+
+                if(board[i][j] == 1){
+                    DFS(i, j);
+                    cnt++;
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Ex1012 main = new Ex1012();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<t; i++){
+
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            board = new int[m][n];  // m행 n열
+
+            for(int j=0; j<k; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                board[x][y] = 1;
+            }
+
+            main.solution();
+
+            bw.write(String.valueOf(cnt) + " ");
+            bw.newLine();
+            cnt=0;
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+
+
+}
