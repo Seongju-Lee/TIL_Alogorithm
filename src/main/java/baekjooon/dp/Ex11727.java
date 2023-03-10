a099e5b4015cac9c6782c1fa25d6f5ce60139519
seongju <lsj40413@gmail.com>
2023-03-10 21:14:15 +0900

    Feat: Solved "Ex11727.java"
    "2×n 타일링 2" 문제풀이
    
    - 접근 방식
    dp를 이용한 풀이.
    11726번과 같은 맥락의 풀이이다.

---
+++ src/main/java/baekjooon/dp/Ex11727.java
@@ -0,0 +1,22 @@
+package baekjooon.dp;
+import java.util.*;
+
+public class Ex11727 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if(n > 1) {
+            dp[2] = 3;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = (dp[i-1] + (dp[i-2]*2))%10_007;
+            }
+        }
+
+        System.out.print(dp[n]%(10_007));
+    }
+}
